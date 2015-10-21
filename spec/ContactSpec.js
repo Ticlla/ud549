0b1f77c7c8cbbe868829ec4cee02b3672a6fd535
Alcides Ticlla <bandycoot_17@hotmail.com>
2015-10-20 22:32:24 -0400

    adds test case
    adds new changes to ContactSpec, prueba, SpecRuner

---
+++ spec/ContactSpec.js
@@ -0,0 +1,12 @@
+
+describe('Name of the group', function() {
+  it('should behave...', function() {
+    expect(true).toBe(true);
+  });
+  it('should behave...', function() {
+    expect(true).toBe(true);
+  });
+  it('should behave...', function() {
+    expect(true).toBe(true);
+  });
+});
---
+++ spec/prueba.js
@@ -0,0 +1,19 @@
+describe('Unit test - Prueba', function() {
+	it('should agragar un elemnto a la l', function() {
+		expect(true).toBe(true);
+	});
+	it('should behave...', function() {
+		expect(true).toBe(false);
+	});
+	it('should behave...', function() {
+		expect(true).toBe(true);
+	});
+	describe('Unit test - Prueba Fucntion', function() {
+		it('should behave...', function() {
+			expect(true).toBe(true);
+		});
+		it('should behave...', function() {
+			expect(true).toBe(false);
+		});
+	});
+});
